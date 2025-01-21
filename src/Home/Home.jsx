e4bf2ad978be7b4f837329d1df69adea71178d0f
Sudip Paul <p452570@gmail.com>
2025-01-21 21:37:29 +0530

    second commit

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,13 +1,55 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { motion, useMotionValue, useSpring, useTransform } from 'framer-motion'
+import Phone from './Phone';
 
 const Home = () => {
 
-  let [name,setName] = useState(undefined) 
+  const x = useMotionValue(0)
+  const y = useMotionValue(0)
+
+
+  const mouseXSpring = useSpring(x)
+  const mouseYSpring = useSpring(y)
+
+  // const [rotationY,setRotationY] = useState(0)
+
+  const rotateX = useTransform(mouseYSpring,
+    [-0.5, 0.5], ['20.5deg', '-20.5deg']
+  )
+  const rotateY = useTransform(mouseXSpring,
+    [-0.5, 0.5], ['-17.5deg', '17.5deg']
+  )
+
+  const handleMouseMove = (e) => {
+
+    // const rect = e.target.getBoundingClientRect()  The e.target.getBoundingClientRect() method returns a DOMRect object that provides information about the size of the element and its position relative to the viewport
+    // const width = rect.width
+    // const height = rect.height
+
+    const viewportWidth = window.innerWidth;
+    const viewportHeight = window.innerHeight;
+
+    const mouseXperc = (e.clientX / viewportWidth) - 0.5
+    const mouseYperc = (e.clientY / viewportHeight) - 0.5
+
+    x.set(mouseXperc)
+    y.set(mouseYperc)
+
+
+    // setRotationY(Number(rotateY.current.replace("deg","")))
+    // console.log(Number(rotationY.replace("deg","")))
+
+    // console.log(mouseXperc,mouseYperc)
+  }
+
+  let [name, setName] = useState(undefined)
 
   return (
 
-    <div className='min-h-svh bg-landingBg mx-[30px] sm:mx-[60px] lg:flex pt-[80px] sm:pt-[168px]'>
+    <div className='min-h-svh bg-landingBg mx-[30px] sm:mx-[60px] lg:flex pt-[80px] sm:pt-[168px]'
+    onMouseMove={handleMouseMove}
+    >
 
       {/* Div 1 */}
       <div className='lg:w-1/2'>
@@ -25,7 +67,7 @@ const Home = () => {
 
           <div className='relative'>
             <span className='absolute text-navLinkGrey top-[17px] left-[9px] text-[1.04rem]'>arraytr.ee/</span>
-            <input type="text" className='h-[20px] py-[30px] rounded-lg pl-[86px] pr-[10px] w-[240px] text-navLinkGrey' placeholder='Your Name' onChange={(e) => setName(e.target.value)}/>
+            <input type="text" className='h-[20px] py-[30px] rounded-lg pl-[86px] pr-[10px] w-[240px] text-navLinkGrey' placeholder='Your Name' onChange={(e) => setName(e.target.value)} />
           </div>
 
           <Link to={`/register/${name}`}><button className='px-[20px] py-[15px] bg-claimBtn font-semibold rounded-full mt-[10px] sm:mt-0'>Claim your Arraytree</button></Link>
@@ -36,8 +78,8 @@ const Home = () => {
       {/* Div 2 */}
       <div className='lg:w-1/2'>
 
-        
-
+        {/* Background picture */}
+        <Phone rotateX={rotateX} rotateY={rotateY}/>
 
       </div>
 
---
+++ src/Home/Phone.jsx
@@ -0,0 +1,100 @@
+import React from 'react'
+import { FaLinkedinIn } from "react-icons/fa";
+import { motion, useMotionValue, useSpring, useTransform } from 'framer-motion'
+import sudipWebpic from '../Assets/sudipWebsite.png'
+
+const Phone = ({rotateX,rotateY}) => {
+    return (
+        <>
+            <motion.div
+                style={{
+                    transformStyle: "preserve-3d", //preserve 3d let us the z axis that is toward or away from us
+                    rotateX,
+                    rotateY,
+                }}
+                className='relative w-[250px] h-[500px] cursor-pointer mx-auto mt-[-100px]'
+                initial={{
+                    rotateY: 0,
+                    // skew:"-6deg",
+                }}
+                animate={{
+                    rotateY: 360,
+                    // skew:0
+                }}
+                transition={{
+                    duration: 0.6,
+                    delay: 3,
+                    repeat: Infinity,
+                    repeatDelay: 3
+                }}
+            >
+
+                <img src="https://imgs.search.brave.com/7wjuDHdtIt2AOgyv5gvEv5bJaGctRtqIApVf0pl7Rh0/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzLzM2L2Iz/LzU5LzM2YjM1OWZh/ZDk4MjlkNGU0OTIy/ZTAyOGE4ZmY5YzJj/LmpwZw" alt=""
+                    className='object-cover w-full h-full rounded-[40px]'
+                />
+
+                {/* profile pic name and link list */}
+                <div className='absolute flex flex-col items-center inset-0 top-[40px] gap-[10px]'>
+
+                    <div className='w-[65px] h-[65px] rounded-full'>
+                        <img src="https://imgs.search.brave.com/kEtn0XE6X4atlwH2kTmpDEGHnBcPLWxTw6xIDQ-9kgE/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMxLmNicmltYWdl/cy5jb20vd29yZHBy/ZXNzL3dwLWNvbnRl/bnQvdXBsb2Fkcy8y/MDI0LzAyL2dpbnRv/a2ktcy1mYW1vdXMt/c3RyYXdiZXJyeS1t/aWxrLXNwZWVjaC1p/bi1naW50YW1hLmpw/Zw" alt=""
+                            className='w-full h-full object-cover rounded-full'
+                        />
+                    </div>
+
+                    <div className='text-center leading-5'>
+                        <p className='text-white font-semibold text-sm'>Sudip Paul</p>
+                        <p className='text-white text-xs'>Professional Web developer</p>
+                    </div>
+
+                </div>
+
+                <ul className='text-center w-[90%] absolute inset-0 top-[165px] left-[12px]'
+                    style={{
+                        transform: "translateZ(35px)",
+                    }}
+                >
+                    <li className='font-semibold bg-white w-full py-[10px] rounded-full text-xs'>Latest Project</li>
+                    <li className='font-semibold bg-white w-full py-[10px] rounded-full mt-[10px] text-xs'>Visit Movie Manager</li>
+                    <li className='font-semibold bg-white w-full py-[10px] rounded-full mt-[10px] text-xs'>Follow on LinkedIn</li>
+                </ul>
+
+                {/* Buttons */}
+                <div className='absolute flex gap-[10px] bottom-[80px] right-[-85px]'
+                    style={{
+                        transform: "translateZ(55px)",
+                    }}
+                >
+                    <button className='text-xl bg-white p-[15px] rounded-full'><FaLinkedinIn /></button>
+                    <button className='text-xl bg-white p-[15px] rounded-full'><FaLinkedinIn /></button>
+                    <button className='text-xl bg-white p-[15px] rounded-full'><FaLinkedinIn /></button>
+                </div>
+
+                {/* sample */}
+                <div className='absolute bottom-[80px] left-[-120px] '
+                    style={{
+                        transform: "translateZ(75px)",
+                    }}
+                >
+                    <div className='w-[220px] h-[150px]'>
+                        <img src={sudipWebpic} alt="" className='object-cover w-full h-full rounded-[30px]' />
+                    </div>
+                </div>
+
+                {/* background prop */}
+                <div className='absolute w-[200px] h-[150px] top-[80px] right-[-120px] z-[-1]'
+                    style={{
+                        transform: "translateZ(-35px)",
+                    }}
+                >
+                    <img src="https://imgs.search.brave.com/oc5k8XLRepkf5f6wvAnMk7g8ZW0xOTFEirLXgt0ah7w/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzLzAzL2Ni/LzI1LzAzY2IyNWJk/ODY1YWMwODZkYTkx/ZWFiZTRjODhjYTE0/LmpwZw" alt=""
+                        className='object-cover w-full h-full rounded-3xl'
+                    />
+                </div>
+
+            </motion.div>
+        </>
+    )
+}
+
+export default Phone
