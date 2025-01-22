2265013eb9a46f4181896a8d6c458cb624e38d4a
Sudip Paul <p452570@gmail.com>
2025-01-22 13:00:09 +0530

    responisve navbar for phone added

--- src/Components/Navbar/SignupBtn.jsx
+++ src/Components/Navbar/SignupBtn.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const SignupBtn = () => {
   return (
     <Link to={'/register/undefined'}>
-    <button className='py-[14px] sm:py-[18px] px-[25px] bg-gray-800 hover:bg-gray-700 text-white rounded-full font-semibold'>Sign up free</button>
+    <button className='py-[12px] sm:py-[18px] px-[20px] sm:px-[25px] bg-gray-800 hover:bg-gray-700 text-white rounded-full font-semibold'>Sign up free</button>
     </Link>
   )
 }
