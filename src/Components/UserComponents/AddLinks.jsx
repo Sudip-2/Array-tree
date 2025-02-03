a265fb20c22209fcb39b40224f40e6d5f8a2637b
Sudip Paul <146758688+Sudip-2@users.noreply.github.com>
2025-02-03 20:10:22 +0530

    added register from react hook form because forgot to add that

--- src/Components/UserComponents/AddLinks.jsx
+++ src/Components/UserComponents/AddLinks.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-const AddLinks = ({icon,color,linkName}) => {
+const AddLinks = ({icon,color,linkName,register}) => {
     return (
         <>
             <div className='flex items-center gap-4'>
                 <span className={`text-4xl ${color}`}>
                     {icon}
                 </span>
-                <input type="text" name={linkName} className='bg-navHoverGrey rounded-lg px-4 py-1' />
+                <input type="text" name={linkName} className='bg-navHoverGrey rounded-lg px-4 py-1' {...register(linkName)}/>
             </div>
         </>
     )
--- src/User/Profile.jsx
+++ src/User/Profile.jsx
@@ -2,29 +2,40 @@ import React, { useState } from 'react'
 import MainHeading from '../Components/AuthComponents/MainHeading'
 import Profilepic from '../Components/UserComponents/Profilepic';
 import SubHeading from '../Components/UserComponents/SubHeading';
-import Backgroundpic from '../Components/UserComponents/Backgroundpic';
+// import Backgroundpic from '../Components/UserComponents/Backgroundpic';
 import Continue from '../Components/AuthComponents/Fullwidthcontbtn'
-import { FaYoutube } from "react-icons/fa";
+// import { FaYoutube } from "react-icons/fa";
 import AddLinks from '../Components/UserComponents/AddLinks';
 import { FaGithub } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
-import { FaSquareXTwitter } from "react-icons/fa6";
+// import { FaSquareXTwitter } from "react-icons/fa6";
 import AddBtnForLinks from '../Components/UserComponents/AddBtnForLinks';
 import { PiLinkSimpleBold } from "react-icons/pi";
+import { useForm } from "react-hook-form";
 
 const Profile = () => {
   const [wordLimit, setWordLimit] = useState(80)
-  const [ytshow, setYtshow] = useState(false)
+  // const [ytshow, setYtshow] = useState(false)
   const [gitshow, setGitshow] = useState(false)
   const [linkedinshow, setLInkedinshow] = useState(false)
-  const [Xshow, setXshow] = useState(false)
+  // const [Xshow, setXshow] = useState(false)
   const [personalLinkshow, setPersoLinkshow] = useState(false)
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = data => {
+    console.log(data)
+  }
 
   return (
     <>
       <div className='min-h-svh bg-white pt-10 px-4 pb-28'>
         <MainHeading text1={"Add profile details"} />
-        <form className='mt-8 flex items-center flex-col'>
+        <form className='mt-8 flex items-center flex-col' onSubmit={handleSubmit(onSubmit)}>
 
           {/* Profile pic */}
           <SubHeading text={"Select a Profile image"} />
@@ -34,13 +45,14 @@ const Profile = () => {
           <div className='mt-8 flex flex-col items-center gap-4'>
 
             <SubHeading text={"Add title and bio"} />
-            <input type="text" placeholder='Enter your name' className='bg-navHoverGrey w-[320px] h-[50px] px-5 rounded-xl' required />
+            <input type="text" placeholder='Enter your name' className='bg-navHoverGrey w-[320px] h-[50px] px-5 rounded-xl' required {...register("userName")}/>
 
             <div className='relative'>
               <textarea name="Bio" id="userBio" rows={4} maxLength={80} className='bg-navHoverGrey w-[320px] rounded-xl p-5' placeholder='Add your bio'
                 onChange={(e) => {
                   setWordLimit(e.target.value.length)
                 }}
+                {...register("Bio")}
               >
               </textarea>
               <span className='absolute bottom-3 right-4 text-gray-400'>{wordLimit}/80</span>
@@ -78,13 +90,13 @@ const Profile = () => {
 
               <div className='mt-8'>
                 <div className={`mt-2 ${linkedinshow ? "block" : "hidden"}`}>
-                  <AddLinks icon={<FaLinkedin />} color={'text-blue-500'} linkName={"LinkedIn"} />
+                  <AddLinks icon={<FaLinkedin />} register={register} color={'text-blue-500'} linkName={"LinkedIn"} />
                 </div>
                 <div className={`mt-2 ${gitshow ? "block" : "hidden"}`}>
-                  <AddLinks icon={<FaGithub />} color={''} linkName={"Github"} />
+                  <AddLinks icon={<FaGithub />} register={register} color={''} linkName={"Github"} />
                 </div>
                 <div className={`mt-2 ${personalLinkshow ? "block" : "hidden"}`}>
-                  <AddLinks icon={<PiLinkSimpleBold />} color={''} linkName={"customLink"} />
+                  <AddLinks icon={<PiLinkSimpleBold />} register={register} color={''} linkName={"customLink"} />
                 </div>
               </div>
 
