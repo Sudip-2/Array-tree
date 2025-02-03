6ed3daa5610fa606040038a6311bd9a16d77fabc
Sudip Paul <146758688+Sudip-2@users.noreply.github.com>
2025-02-03 22:09:39 +0530

    added ai button and reset button

--- src/User/Profile.jsx
+++ src/User/Profile.jsx
@@ -12,9 +12,12 @@ import { FaLinkedin } from "react-icons/fa";
 import AddBtnForLinks from '../Components/UserComponents/AddBtnForLinks';
 import { PiLinkSimpleBold } from "react-icons/pi";
 import { useForm } from "react-hook-form";
+import aistar from '../Assets/Aistar.svg'
+import { RiResetRightFill } from "react-icons/ri";
 
 const Profile = () => {
   const [wordLimit, setWordLimit] = useState(80)
+  const [words, setWords] = useState(0)
   // const [ytshow, setYtshow] = useState(false)
   const [gitshow, setGitshow] = useState(false)
   const [linkedinshow, setLInkedinshow] = useState(false)
@@ -24,7 +27,7 @@ const Profile = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors },setValue
   } = useForm();
 
   const onSubmit = data => {
@@ -42,20 +45,44 @@ const Profile = () => {
           <Profilepic />
 
           {/* Title and Bio */}
-          <div className='mt-8 flex flex-col items-center gap-4'>
+          <div className='mt-8 flex flex-col items-center gap-3'>
 
             <SubHeading text={"Add title and bio"} />
-            <input type="text" placeholder='Enter your name' className='bg-navHoverGrey w-[320px] h-[50px] px-5 rounded-xl' required {...register("userName")}/>
+            <input type="text" placeholder='Enter your name' className='bg-navHoverGrey w-[320px] h-[50px] px-5 rounded-xl' required {...register("userName")} />
 
             <div className='relative'>
-              <textarea name="Bio" id="userBio" rows={4} maxLength={80} className='bg-navHoverGrey w-[320px] rounded-xl p-5' placeholder='Add your bio'
-                onChange={(e) => {
-                  setWordLimit(e.target.value.length)
-                }}
-                {...register("Bio")}
+              <textarea name="Bio" id="userBio" rows={3} maxLength={80} className='bg-navHoverGrey w-[320px] rounded-xl p-5' placeholder='Add your bio'
+                {...register("Bio", {
+                  onChange: (e) => {
+                    setWordLimit(e.target.value.length)
+                    setWords(e.target.value.length)
+                    console.log(e.target.value.length)
+                  }
+                }
+                )}
               >
               </textarea>
               <span className='absolute bottom-3 right-4 text-gray-400'>{wordLimit}/80</span>
+            </div>
+
+            <div className='flex items-center w-full justify-between'>
+              <button className='bg-gray-100 flex items-center px-4 py-2 gap-1 rounded-full outline  outline-2 outline-gray-300 hover:outline-1 duration-400'>
+                <img src={aistar} alt="" /> <span className='font-semibold text-sm'>Write my bio</span>
+              </button>
+              <p className='text-sm'>Powered by AI</p>
+            </div>
+
+            <div className='flex items-center w-full'>
+              <button className={`outline outline-1 outline-gray-300 py-2 px-4 rounded-full flex items-center gap-1 ${words == 0 ? "hidden" : "inline"}`}
+                onClick={() => {
+                  setValue("Bio", ""); // Reset the bio field
+                  setWordLimit(0); // Reset the word count
+                  setWords(0);
+                }}
+              >
+                <RiResetRightFill />
+                Reset
+              </button>
             </div>
 
             {/* <div className='mt-6 flex flex-col items-center gap-6'>
@@ -107,8 +134,8 @@ const Profile = () => {
           <div className='fixed bottom-8 w-1/3 left-[33%]'>
             <Continue />
           </div>
-        </form>
-      </div>
+        </form >
+      </div >
     </>
   )
 }
